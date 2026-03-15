daba704795eb63a9084c0f2e0b62c12eab834837
Xnths <jonathascastilhoes@gmail.com>
2026-03-15 10:28:44 -0300

    fix(tracking): posthog configuration

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,8 +2,6 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_TOKEN!, {
   api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
-  defaults: "2026-01-30",
+  ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
   capture_exceptions: true,
-  debug: process.env.NODE_ENV === "development",
 });
